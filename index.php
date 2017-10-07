90cdcfc4d2a0c39e3d39ffcdb5b3f267b5d34072
Nintendo64bit <31171515+Nintendo64bit@users.noreply.github.com>
2017-10-07 08:16:09 -0400

    Update index.php
    all in 1 file

--- index.php
+++ index.php
@@ -1,66 +1,625 @@
-<?php
-/* made by a reeetard*/
-$name = "Nintendo";
-$last = "Nintendo64";
-$age = '16';
-echo "rule34.xxx is awsome";
-    
+<html>
+<head>
 
-if(!file_exists("data/config/shimmie.conf.php")) {
-	header("Location: install.php");
-	exit;
+<style>
+body {
+	background: #000000
+	color: #999999;
+	font-family: sans-serif;
+	font-size: 12px;
 }
-require_once "core/sys_config.inc.php";
-require_once "core/util.inc.php";
-
-// set up and purify the environment
-_version_check();
-_sanitise_environment();
-
-try {
-	// load base files
-	ctx_log_start("Opening files");
-	$files = array_merge(zglob("core/*.php"), zglob("ext/{".ENABLED_EXTS."}/main.php"));
-	foreach($files as $filename) {
-		require_once $filename;
-	}
-	ctx_log_endok();
-
-	ctx_log_start("Connecting to DB");
-	// connect to the database
-	$database = new Database();
-	$config = new DatabaseConfig($database);
-	ctx_log_endok();
-
-	// load the theme parts
-	ctx_log_start("Loading themelets");
-	foreach(_get_themelet_files(get_theme()) as $themelet) {
-		require_once $themelet;
-	}
-	ctx_log_endok();
-
-	_load_extensions();
-
-	// start the page generation waterfall
-	$page = class_exists("CustomPage") ? new CustomPage() : new Page();
-	$user = _get_user();
-	send_event(new InitExtEvent());
-	if(!is_cli()) { // web request
-		send_event(new PageRequestEvent(@$_GET["q"]));
-		$page->display();
-	}
-	else { // command line request
-		send_event(new CommandEvent($argv));
-	}
-
-	// saving cache data and profiling data to disk can happen later
-	if(function_exists("fastcgi_finish_request")) fastcgi_finish_request();
-	$database->commit();
-	ctx_log_endok();
+span.quote {
+	color:#B8D962;
 }
-catch(Exception $e) {
-	if($database) $database->rollback();
-	_fatal_error($e);
-	ctx_log_ender();
+h1 {
+	font-size: 20pt;
+	text-align: center;
+	letter-spacing: 0px;
+}
+div.title, h1 {
+	color: #32DD72;
+	font-family: Arial, Helvetica, sans-serif;
+}
+div.title p {
+	font-size: 10px;
+}
+a:link, a:visited, p.intro a.email span.name {
+	color:#00FFFF	;
+	text-decoration: none;
+	font-family: sans-serif;
+}
+a:link:hover, a:visited:hover {
+	color: #fff;
+	font-family: sans-serif;
+	text-decoration: none;
+
+}
+a.post_no {
+	color: #00FFFF;
+	text-decoration: none;
+}
+a.post_no:hover {
+	color: #32DD72 !important;
+	text-decoration: underline overline;
+}
+div.post.reply {
+	background: #000000
+	border: #000000id;
+		-webkit-border-radius: 10px;
+		-khtml-border-radius: 10px;
+		-moz-border-radius: 10px;
+		border-radius: 10px;
+
+}
+div.post.reply.highlighted {
+	background: #555;
+	border: transparent 1px solid;
+}
+div.post.reply div.body a:link, div.post.reply div.body a:visited {
+	color: #CCCCCC;
+}
+div.post.reply div.body a:link:hover, div.post.reply div.body a:visited:hover {
+	color: #32DD72;
+}
+p.intro span.subject {
+	font-size: 12px;
+	font-family: sans-serif;
+	color: 	#000000
+	font-weight: 800;
+}
+p.intro span.name {
+	color: #32DD72;
+	font-weight: 800;
+}
+p.intro a.capcode, p.intro a.nametag {
+	color: magenta;
+	margin-left: 0;
+}
+p.intro a.email, p.intro a.email span.name, p.intro a.email:hover, p.intro a.email:hover span.name {
+	color: #00FFFF	;
+}
+input[type="text"], textarea, select {
+	background:	#000000
+	color: 	#00FFFF	;
+	border:	#00FFFF	 1px solid;
+	padding-left: 5px;
+	padding-right: -5px;
+	font-family: sans-serif;
+	font-size: 10pt;
+}
+input[type="password"] {
+	background: #333333;
+	color: 	#00FFFF	;
+	border: #00FFFF	 1px solid;
+}
+form table tr th {
+	background:	#000000;
+	color: #00FFFF	;
+	font-weight: 800;
+	text-align: left;
+	padding: 0;
+}
+div.banner {
+	background: #32DD72;
+	color: #000;
+	text-align: center;
+	width: 250px;
+	padding: 4px;
+	padding-left: 12px;
+	padding-right: 12px;
+	margin-left: auto;
+	margin-right: auto;
+	font-size: 12px;
+}
+div.banner a {
+	color:#000;
+}
+input[type="submit"] {
+	background: #333333;
+	border: #888888 1px solid;
+	color: 	#00FFFF	;
+}
+input[type="submit"]:hover {
+	background: #000000
+	border: #888888 1px solid;
+	color: 	#00FFFF	;
+}
+input[type="text"]:focus {
+	border:#aaa 1px solid;
+}
+p.fileinfo a:hover {
+	text-decoration: underline;
+}
+span.trip {
+	color: 	#00FFFF	;
+}
+div.pages {
+	color: 	#00FFFF	;
+	background: #333333;
+	border: #666666 1px solid;
+	font-family: sans-serif;
+	font-size: 10pt;
+}
+div.pages a.selected {
+	color: #CCCCCC;
+}
+hr {
+	height: 1px;
+	border: #333333 1px solid;
+}
+div.boardlist {
+	color: #00FFFF	;
+	background-color: rgba(12%, 12%, 12%, 0.10);
 }
 
+div.ban {
+	background-color: transparent;
+	border: transparent 0px solid;
+}
+div.ban h2 {
+	background: transparent;
+	color: lime;
+	font-size: 12px;
+}
+table.modlog tr th {
+	background: #333333;
+	color: #AAAAAA;
+}
+
+.desktop-style div.boardlist:nth-child(1) {
+  text-shadow: black 1px 1px 1px, black -1px -1px 1px, black -1px 1px 1px, black 1px -1px 1px;
+}
+
+
+.desktop-style div.boardlist:nth-child(1):hover {
+  background-color: rgba(30%, 30%, 30%, 0.65);
+}
+	
+</style>
+	<meta charset="utf-8">
+	
+	<title>tomokochan</title>
+</head>
+<body>	
+	
+	<img class="banner" src="{{ config.url_banner }}" {% if config.banner_width or config.banner_height %}style="{% if config.banner_width %}width:{{ config.banner_width }}px{% endif %};{% if config.banner_width %}height:{{ config.banner_height }}px{% endif %}" {% endif %}alt="" />
+	
+	<header>
+		<h1>Tomokochan</h1>
+		<div class="subtitle">
+			<p><a href="?/">Return to dashboard</a></p>
+		</div>
+	</header>
+	
+	
+		<div class="pages top">
+wwwwwwwwwwwwweeeeeeeeeeeeeeeeee welcome
+
+			
+		</div>
+	
+	
+	global message <hr /><div class="blotter"></div>test
+	<hr />
+	<form name="postcontrols" action="{{ config.post_url }}" method="post">
+	<input type="hidden" name="board" value="" /><input type="hidden" name="mod" value="1" /> weweweweweweeweweweweeweweewweewweewewweeweewe
+	</form>
+	
+	<div class="pages">
+		 [<a {% if page.selected %}class="selected"weweweweweweeweweweweeweweewweewweewewweeweewe
+		
+			 | <a href="">Catalog</a>
+	</div>
+	
+	
+	<footer>
+		<p class="unimportant" style="text-align:center;"></p>
+	</footer>
+	<script type="text/javascript">
+		ready();
+	{% endraw %}</script>
+	<script>
+	<script>
+<html>
+<style>
+	<style>
+body {
+	background: #000000
+	color: #999999;
+	font-family: sans-serif;
+	font-size: 12px;
+}
+span.quote {
+	color:#B8D962;
+}
+h1 {
+	font-size: 20pt;
+	text-align: center;
+	letter-spacing: 0px;
+}
+div.title, h1 {
+	color: #32DD72;
+	font-family: Arial, Helvetica, sans-serif;
+}
+div.title p {
+	font-size: 10px;
+}
+a:link, a:visited, p.intro a.email span.name {
+	color:#00FFFF	;
+	text-decoration: none;
+	font-family: sans-serif;
+}
+a:link:hover, a:visited:hover {
+	color: #fff;
+	font-family: sans-serif;
+	text-decoration: none;
+
+}
+a.post_no {
+	color: #00FFFF;
+	text-decoration: none;
+}
+a.post_no:hover {
+	color: #32DD72 !important;
+	text-decoration: underline overline;
+}
+div.post.reply {
+	background: #000000
+	border: #000000id;
+		-webkit-border-radius: 10px;
+		-khtml-border-radius: 10px;
+		-moz-border-radius: 10px;
+		border-radius: 10px;
+
+}
+div.post.reply.highlighted {
+	background: #555;
+	border: transparent 1px solid;
+}
+div.post.reply div.body a:link, div.post.reply div.body a:visited {
+	color: #CCCCCC;
+}
+div.post.reply div.body a:link:hover, div.post.reply div.body a:visited:hover {
+	color: #32DD72;
+}
+p.intro span.subject {
+	font-size: 12px;
+	font-family: sans-serif;
+	color: 	#000000
+	font-weight: 800;
+}
+p.intro span.name {
+	color: #32DD72;
+	font-weight: 800;
+}
+p.intro a.capcode, p.intro a.nametag {
+	color: magenta;
+	margin-left: 0;
+}
+p.intro a.email, p.intro a.email span.name, p.intro a.email:hover, p.intro a.email:hover span.name {
+	color: #00FFFF	;
+}
+input[type="text"], textarea, select {
+	background:	#000000
+	color: 	#00FFFF	;
+	border:	#00FFFF	 1px solid;
+	padding-left: 5px;
+	padding-right: -5px;
+	font-family: sans-serif;
+	font-size: 10pt;
+}
+input[type="password"] {
+	background: #333333;
+	color: 	#00FFFF	;
+	border: #00FFFF	 1px solid;
+}
+form table tr th {
+	background:	#000000;
+	color: #00FFFF	;
+	font-weight: 800;
+	text-align: left;
+	padding: 0;
+}
+div.banner {
+	background: #32DD72;
+	color: #000;
+	text-align: center;
+	width: 250px;
+	padding: 4px;
+	padding-left: 12px;
+	padding-right: 12px;
+	margin-left: auto;
+	margin-right: auto;
+	font-size: 12px;
+}
+div.banner a {
+	color:#000;
+}
+input[type="submit"] {
+	background: #333333;
+	border: #888888 1px solid;
+	color: 	#00FFFF	;
+}
+input[type="submit"]:hover {
+	background: #000000
+	border: #888888 1px solid;
+	color: 	#00FFFF	;
+}
+input[type="text"]:focus {
+	border:#aaa 1px solid;
+}
+p.fileinfo a:hover {
+	text-decoration: underline;
+}
+span.trip {
+	color: 	#00FFFF	;
+}
+div.pages {
+	color: 	#00FFFF	;
+	background: #333333;
+	border: #666666 1px solid;
+	font-family: sans-serif;
+	font-size: 10pt;
+}
+div.pages a.selected {
+	color: #CCCCCC;
+}
+hr {
+	height: 1px;
+	border: #333333 1px solid;
+}
+div.boardlist {
+	color: #00FFFF	;
+	background-color: rgba(12%, 12%, 12%, 0.10);
+}
+
+div.ban {
+	background-color: transparent;
+	border: transparent 0px solid;
+}
+div.ban h2 {
+	background: transparent;
+	color: lime;
+	font-size: 12px;
+}
+table.modlog tr th {
+	background: #333333;
+	color: #AAAAAA;
+}
+
+.desktop-style div.boardlist:nth-child(1) {
+  text-shadow: black 1px 1px 1px, black -1px -1px 1px, black -1px 1px 1px, black 1px -1px 1px;
+}
+
+
+.desktop-style div.boardlist:nth-child(1):hover {
+  background-color: rgba(30%, 30%, 30%, 0.65);
+}
+</style>
+<script>
+<div class="post reply" id="reply_{{ post.id }}">
+
+<p class="intro"{% if not index %} id="{{ post.id }}"{% endif %}>
+	<input type="checkbox" class="delete" name="delete_{{ post.id }}" id="delete_{{ post.id }}" />
+	<label for="delete_{{ post.id }}">
+		{% if post.subject|length > 0 %}
+			{# show subject #}
+			<span class="subject">{{ post.subject|bidi_cleanup }}</span> 
+		{% endif %}
+		{% if post.email|length > 0 %}
+			{# start email #}
+			<a class="email" href="mailto:{{ post.email }}">
+		{% endif %}
+		{% set capcode = post.capcode|capcode %}
+		<span {% if capcode.name %}style="{{ capcode.name }}" {% endif %}class="name">{{ post.name|bidi_cleanup }}</span>
+		{% if post.trip|length > 0 %}
+			<span {% if capcode.trip %}style="{{ capcode.trip }}" {% endif %}class="trip">{{ post.trip }}</span>
+		{% endif %}
+		{% if post.email|length > 0 %}
+			{# end email #}
+			</a>
+		{% endif %}
+		{% if capcode %}
+			{{ capcode.cap }}
+		{% endif %}			 [<a class="ip-link" style="margin:0;" href="?/IP/{{ post.ip }}">{{ post.ip }}</a>]
+		{% if config.display_flags and post.modifiers.flag %}
+			 <img class="flag" src="{{ config.uri_flags|sprintf(post.modifiers.flag) }}"
+			 	style="{% if post.modifiers['flag style'] %}{{ post.modifiers['flag style'] }}{% else %}{{ config.flag_style }}{% endif %}"
+			 {% if post.modifiers['flag alt'] %}alt="{{ post.modifiers['flag alt'] | e('html_attr') }}" title="{{ post.modifiers['flag alt'] | e('html_attr') }}"{% endif %}>
+		{% endif %}
+		 <time datetime="{{ post.time|date('%Y-%m-%dT%H:%M:%S') }}{{ timezone() }}">{{ post.time|date(config.post_date) }}</time>
+	</label>
+	{% if config.poster_ids %}
+		 ID: {{ post.ip|poster_id(post.thread) }}
+	{% endif %}
+	 <a class="post_no" onclick="return document.querySelectorAll('div.banner').length ? highlightReply({{ post.id }}) : true;" href="{{ post.link }}">No.</a>
+	<a class="post_no" onclick="return document.querySelectorAll('div.banner').length ? citeReply({{ post.id }}) : true;" href="{{ post.link('q') }}">
+		{{ post.id }}
+	</a>
+	</p>
+	{% if post.embed %}
+		{{ post.embed }}
+	{% elseif post.file == 'deleted' %}
+		<img class="post-image deleted" src="{{ config.image_deleted }}" alt="" />
+	{% elseif post.file and post.file %}
+		<p class="fileinfo">File: <a href="{{ config.uri_img }}{{ post.file }}">{{ post.file }}</a> <span class="unimportant">
+		(
+			{% if post.thumb == 'spoiler' %}
+				Spoiler Image, 
+			{% endif %}
+			{{ post.filesize|filesize }}
+			{% if post.filewidth and post.fileheight %}
+				, {{ post.filewidth}}x{{ post.fileheight }}
+				{% if config.show_ratio %}
+					, {{ post.ratio }}
+				{% endif %}
+			{% endif %}
+			{% if config.show_filename and post.filename %}
+				, 
+				{% if post.filename|length > config.max_filename_display %}
+					<span class="postfilename" title="{{ post.filename|e|bidi_cleanup }}">{{ post.filename|truncate_filename(config.max_filename_display)|e|bidi_cleanup }}</span>
+				{% else %}
+					<span class="postfilename">{{ post.filename|e|bidi_cleanup }}</span>
+				{% endif %}
+			{% endif %}
+			{% if post.thumb != 'file' and config.image_identification %}
+				,
+				<span class='image_id'>
+				 <a href="http://imgops.com/{{ config.domain }}{{ config.uri_img }}{{ post.file }}">io</a>
+				{% if post.file|extension == 'jpg' %}
+					 <a href="http://regex.info/exif.cgi?url={{ config.domain }}{{ config.uri_img }}{{ post.file }}">e</a>
+				{% endif %}
+				 <a href="http://www.google.com/searchbyimage?image_url={{ config.domain }}{{ config.uri_img }}{{ post.file }}">g</a>
+				 <a href="http://www.tineye.com/search?url={{ config.domain }}{{ config.uri_img }}{{ post.file }}">t</a>
+				</span>
+			{% endif %}
+
+		)
+		</span>
+</p>
+	<a href="{{ config.uri_img }}{{ post.file }}" target="_blank"{% if post.thumb == 'file' or post.modifiers['is_file'] == '1' %} class="file"{% endif %}>
+	<img class="post-image" src="
+		{% if post.thumb == 'file' %}
+			{{ config.root }}
+			{% if config.file_icons[post.filename|extension] %}
+				{{ config.file_thumb|sprintf(config.file_icons[post.filename|extension]) }}
+			{% else %}
+				{{ config.file_thumb|sprintf(config.file_icons.default) }}
+			{% endif %}
+		{% elseif post.thumb == 'spoiler' %}
+			{{ config.root }}{{ config.spoiler_image }}
+		{% else %}
+			{{ config.uri_thumb }}{{ post.thumb }}
+		{% endif %}" style="width:{{ post.thumbwidth }}px;height:{{ post.thumbheight }}px" alt="" />
+	</a>
+	{% endif %}
+	{{ post.postControls }}
+	<div class="body">
+		{% endfilter %}{% if index %}{{ post.body|truncate_body(post.link) }}{% else %}{{ post.body }}{% endif %}{% filter remove_whitespace %}
+		{% if post.modifiers['ban message'] %}
+			{{ config.mod.ban_message|sprintf(post.modifiers['ban message']) }}
+		{% endif %}
+	</div>
+</div>
+<br/>
+{% endfilter %}
+</script>
+<form name="post" onsubmit="return dopost(this);" enctype="multipart/form-data" action="{{ config.post_url }}" method="post">
+{{ antibot.html() }}
+{% if id %}<input type="hidden" name="thread" value="{{ id }}">{% endif %}
+{{ antibot.html() }}
+<input type="hidden" name="board" value="Tomokochan">
+{{ antibot.html() }}
+{% if current_page %}
+	<input type="hidden" name="page" value="{{ current_page }}">
+{% endif %}
+{% if mod %}<input type="hidden" name="mod" value="1">{% endif %}
+	<table>
+		{% if not config.field_disable_name or (mod and post.mod|hasPermission(config.mod.bypass_field_disable, board.uri)) %}<tr>
+			<th>
+				Name			</th>
+			<td>
+				<input type="text" name="name" size="25" maxlength="35" autocomplete="off">
+			</td>
+		</tr>{% endif %}
+		{% if not config.field_disable_email or (mod and post.mod|hasPermission(config.mod.bypass_field_disable, board.uri)) %}<tr>
+			<th>
+				Email
+			</th>
+			<td>
+				<input type="text" name="email" size="25" maxlength="40" autocomplete="off">
+			</td>
+		</tr>{% endif %}
+		<tr>
+			{% if not (config.field_disable_subject or (id and config.field_disable_reply_subject)) or (mod and post.mod|hasPermission(config.mod.bypass_field_disable, board.uri)) %}<th>
+				Subject
+			</th>
+			<td>
+				<input style="float:left;" type="text" name="subject" size="25" maxlength="100" autocomplete="off">
+			<th>
+				Submit
+			</th>
+			<td>
+				<input accesskey="s" style="margin-left:2px;" type="submit" name="post" value="   button reply " /> <input id="spoiler" name="spoiler" type="checkbox"> <label for="spoiler">Spoiler Image</label>
+			</td>
+		</tr>
+		<tr>
+			<th>
+				Comment
+			</th>
+			<td>
+				<textarea name="body" id="body" rows="5" cols="35"></textarea>
+			</td>
+		</tr>
+		<tr>
+			<th>
+			</th>
+			<td>
+				<script type="text/javascript" src="//www.google.com/recaptcha/api/challenge?k={{ config.recaptcha_public }}"></script>
+			</td>
+	
+			<th>
+				File
+			</th>
+			<td>
+				<input type="file" name="file">
+					<br>
+					<div style="float:none;text-align:left">
+						<label for="file_url">Or URL</label>: 
+						<input style="display:inline" type="text" id="file_url" name="file_url" size="35">
+					</div>
+				
+			</td>
+		</tr>
+		 if config.enable_embedding 
+		<tr>
+			<th>
+			</th>
+			<td>
+				<input type="text" name="embed" size="30" maxlength="120" autocomplete="off">
+			</td>
+		</tr>
+		
+		if mod and not id and post.mod|hasPermissions or (not id and post.mod|hasPermission or post.mod|hasPermission
+		<tr>
+			<th>
+				Flags
+			</th>
+			<td>
+				if not id and post.mod|hasPermission config.mod.sticky, board.uri <div>
+					<label for="sticky">Sticky</label>
+					<input title="Sticky" type="checkbox" name="sticky" id="sticky"><br>
+				</div>
+				 if not id and post.mod|hasPermissionconfig.mod.lock, board.uri<div>
+					<label for="lock">Lock</label><br>
+					<input title="Lock" type="checkbox" name="lock" id="lock">
+				</div>
+				if post.mod|hasPermission config.mod.rawhtml, board.uri<div>
+					<label for="raw">Raw HTML</label><br>
+					<input title="Raw HTML" type="checkbox" name="raw" id="raw">
+				</div>
+			</td>
+		</tr>
+		password or post.mod|hasPermission<tr>
+			<th>
+				Password
+			</th>
+			<td>
+				<input type="password" name="password" size="12" maxlength="18" autocomplete="off"> 
+				<span class="unimportant">For file deletion.</span>
+			</td>
+		</tr>
+	</table>
+
+<input type="hidden" name="hash" value="{{ antibot.hash() }}">
+</form>
+
+<script type="text/javascript">{% raw %}
+	rememberStuff();
+{% endraw %}</script>
+		<p class="unimportant" style="margin-top:20px;text-align:center;"><a href="http://github.com/Nintendo64bit/">Created by a retard</a>  |  Copyright &copy; 2017 tomokochan Development Group</p>
+
+</html>
+</script>
+</body>
+</html>
